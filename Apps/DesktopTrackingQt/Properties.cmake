4f4ae519f1e518705581a4839f23ebff5e7deb7a
atrog <none@none>
2016-07-13 11:53:50 +0200

    Added 1st version of DestktopTrackingQt.
    --HG--
    branch : fw4spl_0.11.0

---
+++ Apps/DesktopTrackingQt/Properties.cmake
@@ -0,0 +1,43 @@
+
+set( NAME DesktopTrackingQt )
+set( VERSION 0.1 )
+set( TYPE APP )
+set( DEPENDENCIES
+)
+set( REQUIREMENTS 
+    gui
+    guiQt
+    
+    appXml
+    
+    dataReg
+    arDataReg
+
+    ctrlCamp
+
+    servicesReg
+    fwlauncher
+    vtkSimpleMesh
+    visuVTK
+    visuVTKQt
+    visuVTKAdaptor
+    visuVTKARAdaptor
+
+    io
+    ioVTK
+    ioAtoms
+    
+    ctrlCamera
+    videoTools
+    videoQt
+    maths
+    
+    tracker
+    trackerAruco
+    patchMedicalData
+
+    droidTools
+    uiData
+)
+
+bundleParam(appXml PARAM_LIST config PARAM_VALUES DesktopTrackingQtConfig)
