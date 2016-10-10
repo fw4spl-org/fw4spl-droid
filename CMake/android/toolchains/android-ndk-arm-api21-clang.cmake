c989faaadea93f5650a5a1fe82d1308b5b3f0774
Julien Waechter <julien.waechter@gmail.com>
2016-10-10 16:41:53 +0200

    Added new toolchain for Android armv7-a API level 21 (only for CMake 3.7)
    --HG--
    branch : fw4spl_0.11.0

---
+++ CMake/android/toolchains/android-ndk-arm-api21-clang.cmake
@@ -0,0 +1,18 @@
+# Android CLang toolchain file for Android arch armv7-a, API level 21
+
+cmake_minimum_required(VERSION 3.7)
+
+set(CMAKE_SYSTEM_NAME Android)
+set(CMAKE_SYSTEM_VERSION 21) # API level
+set(CMAKE_ANDROID_ARCH_ABI armeabi-v7a)
+set(CMAKE_ANDROID_ARM_NEON on)
+# Find Android NDK
+if(DEFINED ENV{ANDROID_NDK})
+    set(CMAKE_ANDROID_NDK "$ENV{ANDROID_NDK}" CACHE PATH "Path to the Android NDK")
+else()
+    message(FATAL_ERROR "Can not find android NDK path, please set the path in env var 'ANDROID_NDK'")
+endif()
+set(CMAKE_ANDROID_STL_TYPE c++_shared)
+set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION clang)
+remove_definitions( -DANDROID )
+add_definitions( -DANDROID )
