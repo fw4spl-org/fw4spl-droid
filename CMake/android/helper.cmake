f09c9d8862be5ae48349121d01a6e625ccbc2b65
Flavien Bridault <fbridault@ircad.fr>
2017-09-13 16:27:16 +0200

    refactor(fwGuiQt): change qt plugin path to lib/qt5/plugins

--- CMake/android/helper.cmake
+++ CMake/android/helper.cmake
@@ -17,7 +17,7 @@ macro(add_push_lib)
         add_custom_target(${FWPROJECT_NAME}_push
             COMMAND ${ADB_PRG} push ${CMAKE_BINARY_DIR}/${LIBRARY_OUTPUT_DIR}/lib${FWPROJECT_NAME}.so /sdcard/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}/lib${FWPROJECT_NAME}.so
             # COMMAND ${ADB_PRG} shell "su -c 'rm -rf /data/data/${PACKAGE_DOT}/lib/lib${FWPROJECT_NAME}.so'"
-            COMMAND ${ADB_PRG} shell "su -c 'mv -v /storage/emulated/0/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}/lib${FWPROJECT_NAME}.so /data/data/${PACKAGE_DOT}/plugins/platforms/lib${FWPROJECT_NAME}.so'"
+            COMMAND ${ADB_PRG} shell "su -c 'mv -v /storage/emulated/0/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}/lib${FWPROJECT_NAME}.so /data/data/${PACKAGE_DOT}/lib/qt5/plugins/platforms/lib${FWPROJECT_NAME}.so'"
             # COMMAND ${ADB_PRG} shell "su -c 'chmod 755 /data/data/${PACKAGE_DOT}/lib/lib${FWPROJECT_NAME}.so'"
             #
             COMMAND ${ADB_PRG} shell "su -c 'rm -r /storage/emulated/0/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}'"
@@ -32,25 +32,25 @@ macro(add_push_bundle)
         get_filename_component(EXTENSION ${CURRENT_FILE} EXT)
         if( NOT ${EXTENSION} STREQUAL ".so" AND NOT ${EXTENSION} STREQUAL ".DS_Store" )
             get_filename_component(FILENAME ${CURRENT_FILE} NAME)
-            
+
             string(LENGTH ${CURRENT_FILE} CURRENT_LENGTH)
             string(LENGTH ${FWPROJECT_NAME}_${DASH_VERSION} PRJ_NAME_LENGTH)
-            
-            
+
+
             string(FIND ${CURRENT_FILE} "${FWPROJECT_NAME}_${DASH_VERSION}" PRJ_NAME_POS REVERSE)
             MATH( EXPR PRJ_NAME_POS "${PRJ_NAME_POS} + ${PRJ_NAME_LENGTH} + 1" )
-             
+
             MATH( EXPR FINAL_LENGTH "${CURRENT_LENGTH} - ${PRJ_NAME_POS}" )
             string(SUBSTRING ${CURRENT_FILE} ${PRJ_NAME_POS} ${FINAL_LENGTH} SUB_DIR )
-            
+
             if(NOT ${SUB_DIR} STREQUAL "AndroidManifest.xml")
                 set(BUNDLE_RC "${BUNDLE_RC}mv -v /storage/emulated/0/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}/${SUB_DIR} /data/data/${PACKAGE_DOT}/Bundles/${FWPROJECT_NAME}_${DASH_VERSION}/${SUB_DIR}\;")
             endif()
 
         endif()
-        
+
     endforeach()
-    
+
     set(BUNDLE_RC "${BUNDLE_RC}'")
     if(EXISTS ${BUNDLE_DIR}/lib${FWPROJECT_NAME}_${DASH_VERSION}.so )
         add_custom_target(${FWPROJECT_NAME}_push
@@ -63,7 +63,7 @@ macro(add_push_bundle)
         #     COMMAND ${ADB_PRG} shell "su -c 'chmod 700 /data/data/${PACKAGE_DOT}/lib/lib${FWPROJECT_NAME}_${DASH_VERSION}.so'"
             # COMMAND ${ADB_PRG} shell "su -c 'chmod 755 /data/data/${PACKAGE_DOT}/lib/lib${FWPROJECT_NAME}_${DASH_VERSION}.so'"
             COMMAND ${ADB_PRG} shell "${BUNDLE_RC}"
-        
+
             # COMMAND ${ADB_PRG} shell "su -c 'rm -r /sdcard/fw4spl_tmp/${FWPROJECT_NAME}_${DASH_VERSION}'"
         )
     else()
